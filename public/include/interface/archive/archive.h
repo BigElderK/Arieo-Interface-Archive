f61668b6f5ee64725d93654644e278c8f3759e23
BigElderK <BigElderK@divineland.org>
2026-02-07 12:41:01 +0800

    init commit

---
+++ public/include/interface/archive/archive.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <filesystem>
+namespace Arieo::Interface::Archive
+{
+    class IArchive
+    {
+    public:
+        virtual std::tuple<void*, size_t> getFileBuffer(const std::filesystem::path& relative_path) = 0;
+    };
+
+    class IArchiveManager
+    {
+    public:
+        virtual IArchive* createArchive(const std::filesystem::path& root_path) = 0;
+        virtual void destroyArchive(IArchive*) = 0;
+    };
+}
