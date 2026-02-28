d83fe70a61e8d0f62144e8ee5c9ebd8630f07922
BigElderK <BigElderK@divineland.org>
2026-02-28 23:09:36 +0800

    Fix interface return types for IArchiveManager and namespace qualifications for IImageBuffer

--- public/include/interface/archive/archive.h
+++ public/include/interface/archive/archive.h
@@ -3,17 +3,10 @@
 
 namespace Arieo::Interface::Archive
 {
-    class IFileBuffer
-    {
-    public:
-        virtual void* getBuffer() = 0;
-        virtual size_t getBufferSize() = 0;
-    };
-
     class IArchive
     {
     public:
-        virtual Base::Interop::SharedRef<IFileBuffer> aquireFileBuffer(Base::Interop::StringView related_path) = 0;
+        virtual Base::Interop::SharedRef<Base::IBuffer> aquireFileBuffer(Base::Interop::StringView related_path) = 0;
     };
 
     class IArchiveManager
